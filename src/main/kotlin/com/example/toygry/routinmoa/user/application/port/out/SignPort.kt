c21a697572a28866cf42a48a0352dd61af32d1f5
jungry <seojungrain@gmail.com>
2025-02-27 12:18:39 +0900

    token 개발중

--- src/main/kotlin/com/example/toygry/routinmoa/user/application/port/out/SignPort.kt
+++ src/main/kotlin/com/example/toygry/routinmoa/user/application/port/out/SignPort.kt
@@ -6,6 +6,6 @@ interface SignPort {
     fun login(email: String): SignResult
     fun signUp(email: String): SignResult
     fun sendEmail(email: String, type: String): SignResult
-    fun verifyCode(email: String, code: String): Boolean
+    fun verifyCode(email: String, code: String): SignResult
     fun setName(email: String, name: String): SignResult
 }
